aec7a1bf38d39cb08473f4964fe67fd7831f6158
Neiman22 <pushkarev_av@inbox.ru>
2022-11-04 16:55:23 +0300

    add: code in index.js in 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,12 @@
-
+const fs = require('fs');
+const dirCopy = './files-copy/';
+fs.mkdir(dirCopy, { recursive: true }, (err) => {
+    if (err) throw err;
+    fs.readdir('./files/',(err, files) => {
+        for (let i = 0; i < files.length; i++) {
+            src = './files/' + files[i];
+            dest = './files-copy/' + files[i];
+            fs.copyFile (src, dest, () => {})
+        }
+    });   
+});
