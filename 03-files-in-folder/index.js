7f64cf479d1662f455ebebd90ee45000ba9d2c71
Neiman22 <pushkarev_av@inbox.ru>
2022-11-04 16:20:30 +0300

    add: code in index.js file in 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,19 +1,14 @@
 const fs = require('fs');
 
-fs.stat(`./secret-folder/script.js`, (err, stats) => {
-    console.log (stats.size);
-});
-
-fs.readdir('./secret-folder', (err, files) => {
-    
-    for (i = 0; i < files.length; i++) {
-        fileName = files[i].split('.')[0];
-        fileExtension = files[i].split('.')[1];
-        folder = `./secret-folder/${files[i].toString()}`;
-        console.log (folder);
-        fileSize = fs.stat(folder, (err, stats) => {
-            stats.size;
-        });
-        console.log (`${fileName} - ${fileExtension} - ${fileSize}kb`);
+fs.readdir('./secret-folder/',(err, files) => {
+    for (let i = 0; i < files.length; i++) {
+        folder = './secret-folder/' + files[i];
+        fs.stat(folder, (err, stats) => {
+            if (stats.isFile()) {
+                console.log (`${files[i].split('.')[0]} - ${files[i].split('.')[1]} - ${stats.size} byte`);
+            };
+        })  
     }
 })
+
+
