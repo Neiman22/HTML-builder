acb7fc5cfb3b553e8423cbe86d5c51f846515474
Neiman22 <pushkarev_av@inbox.ru>
2022-11-05 13:18:57 +0300

    add: code in index.js in 05-merge-styles folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,7 +4,7 @@ const dirPath = path.join(__dirname, '/secret-folder/');
 
 fs.readdir(dirPath,(err, files) => {
     for (let i = 0; i < files.length; i++) {
-        folder = dirPath + files[i];
+        folder = path.join(dirPath, files[i]);
         fs.stat(folder, (err, stats) => {
             if (stats.isFile()) {
                 console.log (`${files[i].split('.')[0]} - ${files[i].split('.')[1]} - ${stats.size} byte`);
